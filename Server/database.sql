1ef0b91361f6a29c27993d0ed0fca30120cea520
Manan Singhal <80479207+manan-singhal@users.noreply.github.com>
2022-03-13 15:56:56 +0530

    Update database.sql
    Employee table column data

--- Server/database.sql
+++ Server/database.sql
@@ -1,9 +1,35 @@
 CREATE DATABASE ltc
 
 CREATE TABLE employee(
+    requestId SERIAL,
     FirstName VARCHAR(50),
     LastName VARCHAR(50),
     EmpCode INTEGER,
     Designation VARCHAR(50),
-    Department VARCHAR(20)
+    Department VARCHAR(20),
+    joindate date,
+    bandpay VARCHAR,
+    leaveNature VARCHAR,
+    leaveFrom date,
+    leaveTo date,
+    leaveNoOfDays INTEGER,
+    prefixFrom date,
+    prefixTo date,
+    suffixFrom date,
+    suffixTo date,
+    spouse VARCHAR,
+    selfFrom date,
+    selfTo date,
+    familyFrom date,
+    familyTo date,
+    homeTown VARCHAR,
+    natureOfLTC VARCHAR,
+    placeName VARCHAR,
+    estimatedFare INTEGER,
+    advancedRequired VARCHAR,
+    encashmentRequired VARCHAR,
+    encashmentDays INTEGER,
+    blockYear VARCHAR,
+    ltcTable VARCHAR,
+    requestStatus VARCHAR
 );
